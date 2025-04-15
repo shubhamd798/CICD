b16ee62b2646a40392568d90f4d0cf4d17dde325
Shubham Deshmukh <shubh@UBVPNN10005.core.unionbank.com>
2025-04-14 22:56:38 -0700

    Passed1

--- DevSecOps-secure-app/tests/test_main.py
+++ DevSecOps-secure-app/tests/test_main.py
@@ -1,7 +1,15 @@
-from app.main import greet  # ← importing your function from main.py
+from app.main import greet
+from app.utils import format_message, is_valid_name
 
 def test_greet_valid():
-    assert greet("Shubham") == "Hello, Shubham!"  # success test
+    assert greet("DevSecOps") == "Hello, DevSecOps!"
 
 def test_greet_invalid():
-    assert greet("") == "Invalid name."  # fail case test
+    assert greet("") == "Invalid name."
+
+def test_format_message():
+    assert format_message("Shubham") == "Hello, Shubham!"
+
+def test_is_valid_name():
+    assert is_valid_name("Shubham") is True
+    assert is_valid_name(" ") is False
